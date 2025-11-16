cc01149f11caa3a30eb35d51fd3ce7af366268cb
Ibrahim Khalil <ibrahimkhalil@Ibrahims-MacBook-Air-2.local>
2025-11-17 01:02:55 +0530

    _site pushed

---
+++ _site/assets/js/data/swconf.js
@@ -0,0 +1,54 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1763321197',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/categories/',
+      
+        '/faqs/',
+      
+        '/grievance/',
+      
+        '/help/',
+      
+        '/resources/',
+      
+        '/trajectories/',
+      
+        '/whatsapp-groups/',
+      
+        '/opportunities/',
+      
+        '/tags/',
+      
+        '/archives/',
+      
+        '/about/',
+      
+        '/certifications/',
+      
+        '/projects/',
+      
+        '/alumni/',
+      
+        '/alumni/2025/',
+      
+        '/alumni/2026/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
