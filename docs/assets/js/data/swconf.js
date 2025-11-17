25c835d12eb8acdc0f7813ea15dffacee40da5a1
Ibrahim Khalil <ibrahimkhalil@Ibrahims-MacBook-Air-2.local>
2025-11-17 05:57:23 +0530

    final from ibrahim side, alumni pages added, menu fixed, duck changed

--- docs/assets/js/data/swconf.js
+++ docs/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1763339006',resources: [
+    cacheName: 'chirpy-1763339237',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
